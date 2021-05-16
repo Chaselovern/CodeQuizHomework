a6bdc1ba7a29699803edfee09843b37e7cc0c20f
Chase Lovern <Chaselovern@gmail.com>
2021-05-16 15:55:53 -0600

    added questions.js

---
+++ Questions.js
@@ -0,0 +1,79 @@
+var questions = [{
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""  },
+  {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""
+  },
+  {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""
+  },
+  {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""
+  },
+  {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""
+  },
+  {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""
+  },  {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""
+  },
+  {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""
+  },
+  {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""
+  },
+  {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    answer: ""
+  }
+];
